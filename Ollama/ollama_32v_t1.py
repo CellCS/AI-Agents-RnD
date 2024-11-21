da2edfc80831f300d14661611915077bb99a4928
gregory <gregory@mbp.home>
2024-11-21 02:05:14 -0500

    update with using url request

--- Ollama/ollama_32v_t1.py
+++ Ollama/ollama_32v_t1.py
@@ -7,8 +7,10 @@ from PIL import Image
 
 # ollama run llama3.2-vision
 # use 'ollama list' get model name
+# https://github.com/ollama/ollama/blob/main/docs/api.md
 VISION_MODEL = 'llama3.2-vision'
-OLLAMA_CHAT_URL="http://localhost:8080/chat"
+OLLAMA_CHAT_URLS=["http://localhost:11434/api/chat", "http://localhost:11434/api/generate",]
+OLLAMA_CHAT_URL="http://localhost:11434/api/chat"
 
 SYSTEM_PROMPT = """Act as an OCR assistant. Analyze the provided image and:
 1. Recognize all visible text in the image as accurately as possible.
@@ -30,25 +32,28 @@ def perform_ocr_imagepath(image_path, systemprompt, usingdefault:bool):
     
 def perform_ocr(base64_image, systemprompt):
     """Using request with api url."""
-    response = requests.post(
-        OLLAMA_CHAT_URL,
-        json={
-            "model": VISION_MODEL,
-            "messages": [
-                {
-                    "role": "user",
-                    "content": systemprompt,
-                    "images": [base64_image],
-                },
-            ],
-        }
-    )
-    if response.status_code == 200:
-        pprint.pprint(response.json().get("message", {}).get("content", ""))
-    else:
-        pprint.pprint("Error:", response.status_code, response.text)
-        return None
-    return response
+    try:
+        response = requests.post(
+            OLLAMA_CHAT_URL,
+            json={
+                "model": VISION_MODEL,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": systemprompt,
+                        "images": [base64_image],
+                    },
+                ],
+                "stream":False
+            }
+        )
+        if response.status_code == 200:
+            pprint.pprint(response.content)
+        else:
+            pprint.pprint("Error:", response.content)
+        return response
+    except Exception as e:
+        return e
     
 def perform_ocr_default(base64_image, systemprompt):
     """Using ollama lib to call."""
@@ -66,11 +71,12 @@ def perform_ocr_default(base64_image, systemprompt):
     
 if __name__ == "__main__":
     image_path = "./assets/oneform.png"
+    context_jsonfile='./assets/formsetup.json'
     # get plate setup basic info
-    with open('./assets/formsetup.json') as f:
+    with open(context_jsonfile) as f:
         formsetupstring = json.load(f)
-    systemprompt = f'''Given a CRF form image and a corresponding form setup JSON content with field setup information. Here is setup info: {formsetupstring}. Please analyze the image to identify each field and its corresponding value. For checkboxes or choice fields, the setup JSON file provides the available choices, with the value indicating whether each choice is selected or unselected. Please return a JSON object with the filled field values from the provided form image. Thank you for your assistance.'''
-    response = perform_ocr_imagepath(image_path, systemprompt, True)
+    systemprompt = f'''Given a CRF form image and a corresponding form setup JSON content with field setup information. Here is setup info: {formsetupstring}. Please analyze the image to identify each field and its corresponding value. For checkboxes or choice fields, the setup JSON file provides the available choices, with the value indicating whether each choice is selected or unselected. Please return a JSON object with the filled field values from the provided form image.'''
+    response = perform_ocr_imagepath(image_path, systemprompt, False)
     if response:
         pprint.pprint(response)
     
